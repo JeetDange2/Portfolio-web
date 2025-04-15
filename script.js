82bd99c33359ca533a0824b518bffc4f0cf70878
JeetDange2 <155457742+JeetDange2@users.noreply.github.com>
2025-04-16 03:31:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const sidebarWrapper = document.getElementById('sidebar');
 const sidebarPanel = document.querySelector('.sidebar_open');
 
 toggleButton.addEventListener('click', () => {
-    sidebarWrapper.classList.toggle('active'); // show/hide overlay
-    sidebarPanel.classList.toggle('active');   // slide in/out panel
+    sidebarWrapper.classList.toggle('active'); 
+    sidebarPanel.classList.toggle('active');   
 });
 
 sidebarWrapper.addEventListener('click', (e) => {
@@ -48,3 +48,53 @@ window.addEventListener("scroll", () => {
 function downloadCV(){
     alert("CV is not uploaded yet!");
 }
+
+// img tilt and rotate
+const card = document.querySelector('.img');
+const container = document.querySelector('.img-inner');
+
+container.addEventListener('mousemove', (e) => {
+  const rect = container.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  const centerX = container.offsetWidth / 2;
+  const centerY = container.offsetHeight / 2;
+
+  const rotateX = ((y - centerY) / centerY) * 15;
+  const rotateY = ((x - centerX) / centerX) * 15;
+
+  card.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+container.addEventListener('mouseleave', () => {
+  card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+});
+
+
+// for svg animation
+
+document.addEventListener("DOMContentLoaded", () => {
+  const skillsSection = document.querySelector("#about");
+  const progressCircles = document.querySelectorAll(".progress");
+
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              progressCircles.forEach(circle => {
+                  const fill = circle.querySelector(".fill");
+                  const progress = +circle.dataset.progress;
+                  const circumference = 2 * Math.PI * 35;
+
+                  // Animate from 100% to actual percentage
+                  const offset = circumference - (progress / 100) * circumference;
+                  fill.style.strokeDashoffset = offset;
+              });
+
+              observer.unobserve(skillsSection); // Only once
+          }
+      });
+  }, { threshold: 0.5 });
+
+  observer.observe(skillsSection);
+});
